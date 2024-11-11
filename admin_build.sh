27a742c9db7d2df37a4fa8b3e2ef8c5e672144c7
drbarrett <drbarrett@cmu.edu>
2024-11-11 13:21:26 -0500

    Fix path

--- admin_build.sh
+++ admin_build.sh
@@ -8,7 +8,7 @@ cd website/ || exit 255
 
 # make image urls use absolute paths for the CSD TV screens
 # only needed when generating the live site, local builds don't need this
-find . -path "./*/20*/index.html" -exec ./fix-image-paths.py "{}" \;
+find . -path "./*/20*/index.html" -exec ../fix-image-paths.py "{}" \;
 
 rm -f ../generated-website.zip
 zip -r ../generated-website.zip public/
