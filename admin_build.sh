0d5fa6b1021a4cd597e354c5f19fa8406d48d8a3
drbarrett <drbarrett@cmu.edu>
2024-11-11 13:13:46 -0500

    convert img urls to absolute paths
    Needed for displaying blog posts on the CSD TV screens.

--- admin_build.sh
+++ admin_build.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 
-git checkout "main" || exit 1
+git checkout "zola-upgrade" || exit 1
 git pull || exit 2
 
 cd website/ || exit 255
 ../binaries/use_zola build
+
+# make image urls use absolute paths for the CSD TV screens
+# only needed when generating the live site, local builds don't need this
+find . -path "./*/20*/index.html" -exec fix-image-paths.py "{}" \;
+
 rm -f ../generated-website.zip
 zip -r ../generated-website.zip public/
 cd ../
---
+++ fix-image-paths.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.11
+
+#FIXME: change the absolute path to use the blog host, not the test server
+
+# Only needs to be run from the admin-build script
+
+# Rewrite all image links to be absolute instead of relative. This isn't necessary for
+# the blog itself, but is needed for the poor RSS implementation (don't ask) on the TVs
+# up around the CSD spaces that periodocally display blog content.
+
+# needs to be passed the pth to an index file relative to the website directory
+
+from bs4 import BeautifulSoup
+import re
+import sys
+
+f = sys.argv[1]
+# find the path relative to what will be used on the live blog
+subdir = re.findall('./public/(.*)/index.html', f)[0]
+
+with open(f) as fin:
+    soup = BeautifulSoup(fin, features="html.parser")
+    fin.close()
+
+    for img in soup.findAll('img'):
+        if not img['src'].startswith('https'):
+            if img['src'].startswith('./'):
+                img['src'] = img['src'][2:]
+            img['src'] = f'https://skycat.com/zola/{subdir}/{img["src"]}'
+
+    with open(f, "wb") as fout:
+        fout.write(soup.prettify("utf-8"))
